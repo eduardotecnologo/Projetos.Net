42e7cbd48a753f998bbe860fa0b3396c965cb8a6
Eduardo Alexandre L.Silva <eduardotecnologo@hotmail.com>
2016-09-16 11:04:29 -0300

    Update

---
+++ ERP/ERP_Projeto01/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtNota1 = New System.Windows.Forms.TextBox()
+        Me.txtNota2 = New System.Windows.Forms.TextBox()
+        Me.txtResultado = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(270, 129)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(100, 23)
+        Me.btnCalcular.TabIndex = 0
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(221, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Nota01"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(221, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(42, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Nota02"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(208, 91)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Resultado"
+        '
+        'txtNota1
+        '
+        Me.txtNota1.Location = New System.Drawing.Point(270, 12)
+        Me.txtNota1.Name = "txtNota1"
+        Me.txtNota1.Size = New System.Drawing.Size(100, 20)
+        Me.txtNota1.TabIndex = 4
+        '
+        'txtNota2
+        '
+        Me.txtNota2.Location = New System.Drawing.Point(270, 42)
+        Me.txtNota2.Name = "txtNota2"
+        Me.txtNota2.Size = New System.Drawing.Size(100, 20)
+        Me.txtNota2.TabIndex = 5
+        '
+        'txtResultado
+        '
+        Me.txtResultado.Location = New System.Drawing.Point(270, 88)
+        Me.txtResultado.Name = "txtResultado"
+        Me.txtResultado.Size = New System.Drawing.Size(100, 20)
+        Me.txtResultado.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(382, 254)
+        Me.Controls.Add(Me.txtResultado)
+        Me.Controls.Add(Me.txtNota2)
+        Me.Controls.Add(Me.txtNota1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNota1 As TextBox
+    Friend WithEvents txtNota2 As TextBox
+    Friend WithEvents txtResultado As TextBox
+End Class
---
+++ ERP/ERP_Projeto01/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private dblNota1 As Double
+    Private dblNota2 As Double
+    Private dblResultado As Double
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        If txtNota1.TextLength > 0 Then
+            If IsNumeric(txtNota1.Text) = True Then
+                dblNota1 = Convert.ToDouble(txtNota1.Text)
+            End If
+        End If
+        If txtNota2.TextLength > 0 Then
+            If IsNumeric(txtNota2.Text) = True Then
+                dblNota2 = Convert.ToDouble(txtNota2.Text)
+            End If
+        End If
+        If dblNota1 > 0 And dblNota2 > 0 Then
+            dblResultado = (dblNota1 + dblNota2) / 2
+            txtResultado.Text = FormatNumber(dblResultado, 2)
+        End If
+    End Sub
+End Class
