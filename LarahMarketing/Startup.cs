6afb0df4bb755f14833a93b60d851c19a907b6e4
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-02-26 15:18:45 -0300

    Corrigindo erro do mtsql

--- LarahMarketing/Startup.cs
+++ LarahMarketing/Startup.cs
@@ -28,7 +28,7 @@ namespace LarahMarketing
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+                options.UseMySql(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
---
+++ LarahMarketing/snippets.md
@@ -0,0 +1,3 @@
+##Pomelo.EntityFrameworkCore.MySql
+dotnet add package pomelo.entityframeworkcore.mysql --version 2.2.0
+dotnet restore
