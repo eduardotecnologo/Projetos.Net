da9bfe1261668736ee88a7598a3bc718f70f0d38
eduardotecnologo <eduardotecnologo@hotmail.com>
2020-07-21 18:04:26 -0300

    ValueObjects

---
+++ Balta/PaymentContext/PaymentContext.Domain/Enums/EDocumentTypes.cs
@@ -0,0 +1,9 @@
+namespace PaymentContext.Domain.Enums
+{
+  public enum EDocumentType
+  {
+    CPF = 1,
+    CNPJ = 2,
+    RG = 3
+  }
+}
---
+++ Balta/PaymentContext/PaymentContext.Domain/ValueObjects/Email.cs
@@ -0,0 +1,13 @@
+using PaymentContext.Shared.ValueObjects;
+
+namespace PaymentContext.Domain.ValueObjects
+{
+  public class Email : ValueObject
+  {
+    public Email(string address)
+    {
+      Address = address;
+    }
+    public string Address { get; private set; }
+  }
+}
